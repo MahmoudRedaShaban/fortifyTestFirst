2681e02299a2f6f23207f935de99da20876894b9
Mahmoud Reda <mrmrmr033@gmail.com>
2021-05-09 00:08:07 +0200

    add adminLit3 In Project And init And Start in Project

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/home', 'home')->middleware('auth');
+Route::view('/home', 'adminLite.dashboard')->middleware('auth');
